f51af41d97fbb5319801b2211b5f7611f1a5f26e
Mics <mics@mics.tw>
2018-08-20 16:56:25 +0800

    first commit

---
+++ src/main/java/tw/mics/spigot/plugin/donator/donator/Donator.java
@@ -0,0 +1,17 @@
+package tw.mics.spigot.plugin.donator.donator;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public final class Donator extends JavaPlugin {
+
+    @Override
+    public void onEnable() {
+        // Plugin startup logic
+
+    }
+
+    @Override
+    public void onDisable() {
+        // Plugin shutdown logic
+    }
+}
